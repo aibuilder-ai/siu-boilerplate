00954c8213eb208d977bdeab85b8205726f42224
pouria-bashar <pouria.bashardoust@gmail.com>
2026-02-27 22:27:42 +1100

    Remove placeholder TypeScript definition for CloudflareEnv in Next.js dashboard addon

--- src/templates/cloudflare-monorepo/addons/nextjs-dashboard/apps/dashboard/cloudflare-env.d.ts
+++
@@ -1,9 +0,0 @@
-// This file is a placeholder so TypeScript can resolve CloudflareEnv before
-// you run `pnpm types`. After running `pnpm types`, wrangler will overwrite
-// this file with the generated environment interface.
-
-interface CloudflareEnv {
-  ASSETS: Fetcher;
-  EXAMPLE_WORKER: Fetcher;
-  EXAMPLE_WORKER_URL: string;
-}
